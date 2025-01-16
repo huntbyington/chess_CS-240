acfd9df06aafd63934e8e8fdeabaddd93450621c
huntbyington <hunter.byb@gmail.com>
2025-01-15 21:16:47 -0700

    Updated variable names in PawnMoves

--- shared/src/main/java/chess/moveCalculator/PawnMovesCalculator.java
+++ shared/src/main/java/chess/moveCalculator/PawnMovesCalculator.java
@@ -8,33 +8,33 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> checkMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
         ChessPiece currPiece = board.getPiece(myPosition);
-        int direction;
-        direction = (currPiece.getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1;
+        int myTeam;
+        myTeam = (currPiece.getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1;
 
         //Checks for promotion if not possible returns an area containing a null piece
-        ChessPiece.PieceType[] promotion = getPromotionPieces(myPosition, direction);
+        ChessPiece.PieceType[] promotion = getPromotionPieces(myPosition, myTeam);
 
         //Check forward move
-        ChessPosition newPosition = new ChessPosition((myPosition.getRow() + direction), myPosition.getColumn());
+        ChessPosition newPosition = new ChessPosition((myPosition.getRow() + myTeam), myPosition.getColumn());
         if (board.getPiece(newPosition) == null) {
             for(ChessPiece.PieceType currProm : promotion) {
                 moves.add(new ChessMove(myPosition, newPosition, currProm));
             }
 
             //Check for initial moves
-            newPosition = new ChessPosition((myPosition.getRow() + (2 * direction)), myPosition.getColumn());
-            if(direction == -1 && myPosition.getRow() == 7 && board.getPiece(newPosition) == null) {
+            newPosition = new ChessPosition((myPosition.getRow() + (2 * myTeam)), myPosition.getColumn());
+            if(myTeam == -1 && myPosition.getRow() == 7 && board.getPiece(newPosition) == null) {
                 moves.add(new ChessMove(myPosition, newPosition, null));
             }
-            if(direction == 1 && myPosition.getRow() == 2 && board.getPiece(newPosition) == null) {
+            if(myTeam == 1 && myPosition.getRow() == 2 && board.getPiece(newPosition) == null) {
                 moves.add(new ChessMove(myPosition, newPosition, null));
             }
         }
         
         //Check diagonal capture
         if (myPosition.getColumn() != 8) {
-            newPosition = new ChessPosition((myPosition.getRow() + direction), (myPosition.getColumn() + 1));
-            if ((board.getPiece(newPosition) != null) && (((board.getPiece(newPosition).getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1) != direction)) {
+            newPosition = new ChessPosition((myPosition.getRow() + myTeam), (myPosition.getColumn() + 1));
+            if ((board.getPiece(newPosition) != null) && (((board.getPiece(newPosition).getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1) != myTeam)) {
                 for (ChessPiece.PieceType currProm : promotion) {
                     moves.add(new ChessMove(myPosition, newPosition, currProm));
                 }
@@ -42,8 +42,8 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         }
         //Check diagonal capture
         if (myPosition.getColumn() != 1) {
-            newPosition = new ChessPosition((myPosition.getRow() + direction), (myPosition.getColumn() - 1));
-            if ((board.getPiece(newPosition) != null) && (((board.getPiece(newPosition).getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1) != direction)) {
+            newPosition = new ChessPosition((myPosition.getRow() + myTeam), (myPosition.getColumn() - 1));
+            if ((board.getPiece(newPosition) != null) && (((board.getPiece(newPosition).getTeamColor() == ChessGame.TeamColor.BLACK) ? -1 : 1) != myTeam)) {
                 for (ChessPiece.PieceType currProm : promotion) {
                     moves.add(new ChessMove(myPosition, newPosition, currProm));
                 }
