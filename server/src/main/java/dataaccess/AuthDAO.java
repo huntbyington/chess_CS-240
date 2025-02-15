bbd27b585716dda486f0778401492fe219f45ade
huntbyington <hunter.byb@gmail.com>
2025-02-15 16:00:32 -0700

    Updated AuthData

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -5,8 +5,8 @@ import model.AuthData;
 public interface AuthDAO {
 
     void createAuth(String authToken, String username);
-    AuthData getAuth(String authToken);
-    void deleteAuth(String authToken);
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
     void clear();
 
 }
