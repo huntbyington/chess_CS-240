e184c589599bc7a3912a31b4c4a129da10a9e6d8
huntbyington <hunter.byb@gmail.com>
2025-02-15 14:50:19 -0700

    Added UserDAO

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,5 +1,11 @@
 package dataaccess;
 
-public class UserDAO {
-    
+import model.UserData;
+
+public interface UserDAO {
+
+    UserData createUser(String username, String password, String email) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    UserData clear(String username) throws DataAccessException;
+
 }
