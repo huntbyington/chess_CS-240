ed0e39c8940d0737339123116d4ab889f0a6cba5
huntbyington <hunter.byb@gmail.com>
2025-02-18 21:52:59 -0700

    Added MemoryAuthDataAccess

--- server/src/main/java/dataaccess/MemoryAuthDataAccess.java
+++ server/src/main/java/dataaccess/MemoryAuthDataAccess.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
-public class MemoryAuthDataAccess {
+import model.AuthData;
+
+import java.util.HashMap;
+
+public class MemoryAuthDataAccess implements AuthDAO {
+
+    final private HashMap<String, AuthData> authTokenMap = new HashMap<>();
+
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+        authTokenMap.put(authData.authToken(), authData);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authTokenMap.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokenMap.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        authTokenMap.clear();
+    }
 }
