b5a84770f322ba80eaf4106b6f772a6eead137c0
huntbyington <hunter.byb@gmail.com>
2025-02-27 11:42:24 -0700

    Added logout tests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -37,4 +37,79 @@ public class UserServiceTests {
         }
     }
 
+    @Test
+    @DisplayName("Login Test")
+    public void loginTest() {
+        try {
+            UserData user = new UserData("username", "password", "email");
+            userService.register(user);
+
+            AuthData authData = userService.login(user);
+
+            assert Objects.equals(authData.username(), user.username());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Login Test Wrong Password")
+    public void loginTestWrongPass() {
+        try {
+            UserData user = new UserData("username", "password", "email");
+            userService.register(user);
+
+            UserData newLogin = new UserData("username", "wrongPass", "email");
+            AuthData authData = userService.login(newLogin);
+
+            assert false;
+        } catch (DataAccessException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    @DisplayName("Login Test No Email")
+    public void loginTestNoEmail() {
+        try {
+            UserData user = new UserData("username", "password", "email");
+            userService.register(user);
+
+            UserData newLogin = new UserData("username", "password", "");
+            AuthData authData = userService.login(newLogin);
+
+            assert Objects.equals(authData.username(), user.username());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Logout Test")
+    public void logoutTest() throws DataAccessException {
+        UserData user = new UserData("username", "password", "email");
+        AuthData authData = userService.register(user);
+
+        userService.logout(authData.authToken());
+
+        AuthData actual = memoryAuthDataAccess.getAuth(authData.authToken());
+        assert actual == null;
+    }
+
+    @Test
+    @DisplayName("Logout Test Invalid AuthToken")
+    public void logoutTestInvalidAuthToken() {
+        try {
+            UserData user = new UserData("username", "password", "email");
+            AuthData authData = userService.register(user);
+
+            userService.logout(authData.authToken());
+
+            userService.logout(authData.authToken());
+
+            assert false;
+        } catch (DataAccessException e) {
+            assert true;
+        }
+    }
 }
