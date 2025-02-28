968035706b701c462b1b75a3be36685cbf238820
huntbyington <hunter.byb@gmail.com>
2025-02-28 09:03:34 -0700

    Added GameService test for empty game list

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,4 +1,31 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDataAccess;
+import dataaccess.MemoryGameDataAccess;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class GameServiceTests {
+
+    MemoryGameDataAccess memoryGameDataAccess = new MemoryGameDataAccess();
+    MemoryAuthDataAccess memoryAuthDataAccess = new MemoryAuthDataAccess();
+    GameService gameService = new GameService(memoryGameDataAccess, memoryAuthDataAccess);
+
+    @Test
+    @DisplayName("Game List Test Empty List")
+    public void gameListTestEmptyList() throws DataAccessException {
+        memoryAuthDataAccess.createAuth(new AuthData("authToken", "username"));
+        Collection<GameData> actualGameList = gameService.listGames("authToken");
+
+        Collection<GameData> expectedGameList = new ArrayList<>();
+        
+        assert expectedGameList.equals(actualGameList);
+    }
+
 }
