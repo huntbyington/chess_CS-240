ddd6a71455fe6973f38aeff928bd979aefc5d0de
huntbyington <hunter.byb@gmail.com>
2025-03-08 13:48:05 -0700

    Added MySqlGameDataAccess

--- server/src/main/java/dataaccess/MySqlGameDataAccess.java
+++ server/src/main/java/dataaccess/MySqlGameDataAccess.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class MySqlGameDataAccess {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySqlGameDataAccess implements GameDAO{
+    @Override
+    public void createGame(GameData gameData) throws DataAccessException {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames(String username) throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData gameData) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
