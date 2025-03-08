b7a139da275c83493e0fc455f14a1db75862bf5b
huntbyington <hunter.byb@gmail.com>
2025-03-08 13:50:06 -0700

    Added MySqlUserDataAccess

--- server/src/main/java/dataaccess/MySqlUserDataAccess.java
+++ server/src/main/java/dataaccess/MySqlUserDataAccess.java
@@ -1,4 +1,20 @@
 package dataaccess;
 
-public class MySqlUserDataAccess {
+import model.UserData;
+
+public class MySqlUserDataAccess implements UserDAO{
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+        
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
