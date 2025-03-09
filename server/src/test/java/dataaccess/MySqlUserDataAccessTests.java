40dd59b1e7c19e6c866bd9eceb68229daa0aee44
huntbyington <hunter.byb@gmail.com>
2025-03-08 20:07:51 -0700

    Added clear test

--- server/src/test/java/dataaccess/MySqlUserDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlUserDataAccessTests.java
@@ -1,4 +1,37 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MySqlUserDataAccessTests {
+
+    @AfterAll
+    static void clearUserDatabase() throws DataAccessException {
+        UserDAO userDAO = new MySqlUserDataAccess();
+        userDAO.clear();
+    }
+
+    @Test
+    @DisplayName("SQL Create User Test")
+    public void sqlCreateUserTest() throws DataAccessException {
+        // Test used for initial testing of createUser function
+        UserDAO userDAO = new MySqlUserDataAccess();
+
+        userDAO.createUser(new UserData("username", "password", "email"));
+
+        assert true;
+    }
+
+    @Test
+    @DisplayName("SQL Clear Users Test")
+    public void sqlClearUsersTest() throws DataAccessException {
+        // Test used for initial testing of clearUser function
+        UserDAO userDAO = new MySqlUserDataAccess();
+
+        userDAO.clear();
+
+        assert true;
+    }
 }
