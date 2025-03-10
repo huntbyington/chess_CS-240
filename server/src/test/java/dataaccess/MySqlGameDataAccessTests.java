4e574e751f67b49fd2d5ff4c53a7445896826788
huntbyington <hunter.byb@gmail.com>
2025-03-10 09:33:11 -0600

    Added createGame test

--- server/src/test/java/dataaccess/MySqlGameDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlGameDataAccessTests.java
@@ -1,4 +1,19 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MySqlGameDataAccessTests {
+
+    @Test
+    @DisplayName("SQL Create Game Test")
+    public void sqlCreateGameTest() throws DataAccessException {
+        GameDAO gameDAO = new MySqlGameDataAccess();
+
+        GameData gameData = new GameData(1, "whiteUser", "blackUser", "newGame", new ChessGame());
+
+        gameDAO.createGame(gameData);
+    }
 }
