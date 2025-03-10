9492240424ebea13fc197863c09666356a1904db
huntbyington <hunter.byb@gmail.com>
2025-03-10 09:23:13 -0600

    Added games table SQL initialization

--- server/src/main/java/dataaccess/MySqlGameDataAccess.java
+++ server/src/main/java/dataaccess/MySqlGameDataAccess.java
@@ -2,10 +2,37 @@ package dataaccess;
 
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
+import static dataaccess.DatabaseManager.createDatabase;
+import static dataaccess.DatabaseManager.getConnection;
+
 public class MySqlGameDataAccess implements GameDAO{
+
+    public MySqlGameDataAccess() {
+        try {
+            createDatabase();
+
+            var conn = getConnection();
+            var createTableCommand = """
+                            CREATE TABLE IF NOT EXISTS games (
+                            gameID INT NOT NULL PRIMARY KEY,
+                            whiteUsername VARCHAR(255) NOT NULL,
+                            blackUsername VARCHAR(255) NOT NULL,
+                            gameName VARCHAR(255) NOT NULL,
+                            game longtext NOT NULL
+                            );
+                            """;
+            var createTableStatement = conn.prepareStatement(createTableCommand);
+            createTableStatement.executeUpdate();
+
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void createGame(GameData gameData) throws DataAccessException {
 
