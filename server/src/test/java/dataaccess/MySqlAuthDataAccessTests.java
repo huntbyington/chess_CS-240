850b770b7dbca532f73104efa210e434ad5ff067
huntbyington <hunter.byb@gmail.com>
2025-03-11 21:18:29 -0600

    Added createGame tests

--- server/src/test/java/dataaccess/MySqlAuthDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlAuthDataAccessTests.java
@@ -1,4 +1,18 @@
 package dataaccess;
 
+import model.AuthData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MySqlAuthDataAccessTests {
+
+    @Test
+    @DisplayName("SQL Create Auth Test")
+    public void sqlCreateAuthTest() throws DataAccessException {
+        AuthDAO authDAO = new MySqlAuthDataAccess();
+
+        authDAO.createAuth(new AuthData("authToken", "username"));
+
+        assert true;
+    }
 }
