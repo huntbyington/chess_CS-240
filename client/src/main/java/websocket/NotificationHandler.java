6de58970a344c6c055851c24e6c86a3a7b09f7a3
huntbyington <hunter.byb@gmail.com>
2025-03-20 11:33:51 -0600

    Added websocket in client folder

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -1,4 +1,7 @@
 package websocket;
 
-public class NotificationHandler {
+import webSocketMessages.Notification;
+
+public interface NotificationHandler {
+    void notify(Notification notification);
 }
