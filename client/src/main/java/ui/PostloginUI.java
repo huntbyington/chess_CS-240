53d739b97e4d1882f0e2feb2223cf029e93adb66
huntbyington <hunter.byb@gmail.com>
2025-03-21 15:45:13 -0600

    Added quit and help functions to PostloginUI

--- client/src/main/java/ui/PostloginUI.java
+++ client/src/main/java/ui/PostloginUI.java
@@ -1,4 +1,78 @@
 package ui;
 
+import exception.ResponseException;
+import server.ServerFacade;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class PostloginUI {
+
+    private ServerFacade serverFacade;
+    private boolean signedIn = true;
+
+    public PostloginUI(ServerFacade serverFacade) {
+        this.serverFacade = serverFacade;
+    }
+
+    public String run() {
+        System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (signedIn) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+
+        return result;
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + SET_TEXT_COLOR_WHITE + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+    private String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "quit" -> quit();
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String quit() throws ResponseException {
+        serverFacade.logout();
+
+        signedIn = false;
+        return "quit";
+    }
+
+    private String help() {
+        return (SET_TEXT_COLOR_BLUE + "create <NAME> " + SET_TEXT_COLOR_MAGENTA + "- a game\n" +
+                SET_TEXT_COLOR_BLUE + "list " + SET_TEXT_COLOR_MAGENTA + "- games\n" +
+                SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK] " + SET_TEXT_COLOR_MAGENTA + "- a game\n" +
+                SET_TEXT_COLOR_BLUE + "observe <ID> " + SET_TEXT_COLOR_MAGENTA + "- a game\n" +
+                SET_TEXT_COLOR_BLUE + "logout " + SET_TEXT_COLOR_MAGENTA + "- when you are done\n" +
+                SET_TEXT_COLOR_BLUE + "quit " + SET_TEXT_COLOR_MAGENTA + "- playing chess\n" +
+                SET_TEXT_COLOR_BLUE + "help " + SET_TEXT_COLOR_MAGENTA + "- with possible commands\n");
+
+    }
+
 }
