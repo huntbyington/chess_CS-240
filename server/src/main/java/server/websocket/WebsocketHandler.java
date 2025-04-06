a4b7d76a55c989a406ceebca8e4a897a7ea09bde
huntbyington <hunter.byb@gmail.com>
2025-04-06 15:03:25 -0600

    Added Error, LoadGame, and Notification extensions for ServerMessage

---
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -0,0 +1,11 @@
+package server;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class WebsocketHandler {
+
+
+}
